1e5a421a4f6da14ce58017e44d189730d0af4abd
Denbergvanthijs <denbergvanthijs@gmail.com>
2022-09-13 16:42:52 +0200

    Made baseline predictor into a class

--- baseline.py
+++ baseline.py
@@ -1,12 +1,31 @@
 from typing import Counter
 
-with open("./data/dialog_acts.dat") as file:
-    dialog_acts = file.read().splitlines()
-    dialog_acts_split = [dialog_act.split(" ", 1) for dialog_act in dialog_acts]
 
-# print("Dialog Acts:", dialog_acts_split)
+class Baseline:
+    def __init__(self, filepath_dataset: str):
+        """A baseline model that always predicts the most common intent."""
+        # Read the dataset from local folder
+        with open(filepath_dataset) as file:
+            data = file.read().splitlines()  # Split the dataset into lines
 
-counter = Counter([da[0] for da in dialog_acts_split])
+        # Split each line into two parts: the intent and the utterance
+        # The 1 as the second argument means that we only split the line once
+        self.dataset = [row.split(" ", 1) for row in data]  
 
-print("Counter:", counter)
-print("Most common:", counter.most_common(1))
+        # Make a dictionary of number of intents
+        # Key is the intent, value is the number of occurrences of the intent
+        self.frequencies = Counter([row[0] for row in self.dataset])
+        self.most_common = self.frequencies.most_common(1)[0][0]
+
+    def predict(self, input_sentence: str) -> str:
+        """Predict the intent of the input sentence."""
+        return self.most_common
+
+
+
+if __name__ == "__main__":
+    # Create a Baseline object
+    baseline = Baseline("./data/dialog_acts.dat")
+
+    # Predict the intent of an input sentence
+    print(baseline.predict("I want to book a flight from London to Paris"))

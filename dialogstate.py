843c0d068c9f452797c5156ebacc76822c600a77
Denbergvanthijs <denbergvanthijs@gmail.com>
2022-09-23 20:51:46 +0200

    Added preprocessing function to remove stop words

--- dialogstate.py
+++ dialogstate.py
@@ -4,9 +4,14 @@ import random
 from typing import List
 
 import Levenshtein as lev
+import nltk
 import pandas as pd
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
+
+nltk.download('stopwords')
 
 
 class DialogState:
@@ -16,23 +21,25 @@ class DialogState:
         self.history_states = ["1"]
         self.slots = {"area": None, "food": None, "pricerange": None}
 
-        self.states = ["1", "2", "3", "3.1", "4", "5", "6", "7", "8"]
-        self.intents = ["ack", "affirm", "bye", "confirm", "deny", "hello", "inform",
-                        "negate", "null", "repeat", "reqalts", "reqmore", "request", "restart", "thankyou"]
+        self.states = ("1", "2", "3", "3.1", "4", "5", "6", "7", "8")
+        self.intents = ("ack", "affirm", "bye", "confirm", "deny", "hello", "inform",
+                        "negate", "null", "repeat", "reqalts", "reqmore", "request", "restart", "thankyou")
 
-        self.area = ["north", "south", "west", "east", "centre"]
-        self.food = ['jamaican', 'chinese', 'cuban', 'portuguese', 'australasian', 'moroccan', 'traditional',
+        self.area = ("north", "south", "west", "east", "centre")
+        self.food = ('jamaican', 'chinese', 'cuban', 'portuguese', 'australasian', 'moroccan', 'traditional',
                      'international', 'seafood', 'steakhouse', 'japanese', 'gastropub', 'asian oriental', 'catalan',
                      'north american', 'polynesian', 'french', 'european', 'vietnamese', 'tuscan', 'romanian', 'swiss',
                      'thai', 'british', 'modern european', 'fusion', 'african', 'indian', 'turkish', 'italian', 'korean',
-                     'lebanese', 'persian', 'mediterranean', 'bistro', 'spanish', 'indonesian']
-        self.pricerange = ["expensive", "cheap", "moderate"]
+                     'lebanese', 'persian', 'mediterranean', 'bistro', 'spanish', 'indonesian')
+        self.pricerange = ("expensive", "cheap", "moderate")
+        self.dontcare = ("any", "doesnt matter", 'dont care', 'dontcare')
 
-        self.dontcare = ["any", "doesnt matter", 'dont care', 'dontcare']
         self.state_to_slot = {"2": "area", "3": "food", "4": "pricerange"}
         self.restaurants = []
         self.restaurant_info = pd.read_csv(fp_restaurant_info)
+
         self.max_lev_distance = max_lev_distance
+        self.stopwords = stopwords.words('english')
 
         # Save the model to a pickle file to speedup the loading process
         if not os.path.exists(fp_pickle):
@@ -57,15 +64,22 @@ class DialogState:
 
     def act(self, user_utterance: str) -> None:
         """Determines the intent of current user utterance, fills slots and determines the next state of the dialog."""
-        self.history_utterances.append(user_utterance)
-        self.history_intents.append(self.classify_intent(user_utterance))
+        user_utterance_processed = self.preprocessing(user_utterance)
+        self.history_utterances.append(user_utterance_processed)
+        self.history_intents.append(self.classify_intent(user_utterance_processed))
 
         self.run_state()
         print(user_utterance)
-        self.fill_slots(user_utterance)
+        self.fill_slots(user_utterance_processed)
 
         self.history_states.append(self.determine_next_state())
         print(f"intend={self.history_intents[-1]}; slots={self.slots}")
+
+    def preprocessing(self, user_utterance: str):
+        """Preprocesses the user utterance by tokenizing and removing stopwords."""
+        user_utterance = user_utterance.lower()
+        user_utterance = word_tokenize(user_utterance)
+        return " ".join(word for word in user_utterance if word not in self.stopwords)
 
     def run_state(self) -> None:
         """Runs the current state of the dialog."""
@@ -97,7 +111,8 @@ class DialogState:
 
     def fill_slots(self, user_utterance: str) -> None:
         """Fills the slots with the information from the user utterance."""
-        new_slots = {}  # Dict with slots that will be updated at the end of this function
+        slots_strict = {}  # Dict with slots that directly match the user utterance
+        slots_lev = {}  # Dict with slots that match the user utterance with a maximum Levenshtein distance
 
         # Dict indicating the Levenshtein distance for each slot
         slots_dist = {'area': self.max_lev_distance + 1,
@@ -109,31 +124,30 @@ class DialogState:
             # Dont care --> Return slot based of previous state
             previous_state = self.history_states[-1]
 
-            # If previous question was about area, pricerange or food
+            # If previous question was about {area, pricerange or food}
             # Only then we can use the dontcare word
             if word in self.dontcare and previous_state in ("2", "3", "4"):
-                new_slots[self.state_to_slot.get(previous_state)] = "dontcare"
+                slots_strict[self.state_to_slot.get(previous_state)] = "dontcare"
 
             # If direct match is found, set the slot to the found word
             for category, category_name in zip((self.area, self.pricerange, self.food), ("area", "pricerange", "food")):
                 if word in category:
-                    new_slots[category_name] = word
+                    slots_strict[category_name] = word
 
-        # If strict keyword matching did not work, use Levenshtein distance
-        if not new_slots:
-            for word in user_utterance.split():
-                # Find what category the word belongs to
-                match_word, match_dist, match_category = self.recognize_keyword(word, self.max_lev_distance)
-                # Some errors will be tolerated, e.g. "for" and "north", "the" and "thai"
-                # But Levenshtein by itself is probably not enough
+            # Find what category the word belongs to
+            # Some errors will be tolerated, e.g. "for" and "north", "the" and "thai"
+            # But Levenshtein by itself is probably not enough
+            match_word, match_dist, match_category = self.recognize_keyword(word)
 
-                # Check if for that category, there's already a better match in the sentence.
-                # For each slot, we only want to keep the best match
-                if match_category and match_dist < slots_dist.get(match_category):
-                    slots_dist[match_category] = match_dist  # Make new distance smaller
-                    new_slots[match_category] = match_word  # Update the best match for that category
+            # Check if for that category, there's already a better match in the lev slots
+            # For each slot, we only want to keep the best match
+            if match_word and match_dist < slots_dist.get(match_category):
+                slots_dist[match_category] = match_dist  # Update the distance with the new distance
+                slots_lev[match_category] = match_word  # Update the best match for that category
 
-        self.slots.update(new_slots)  # Update the user preferences based on the user's current utterance
+        # Priority to strict slots, overwrite Levenshtein slots with strict slots
+        slots_lev.update(slots_strict)
+        self.slots.update(slots_lev)  # Update the user preferences based on the user's current utterance
 
     def determine_next_state(self) -> str:
         """Determines the next state of the dialog based on the current state, filled slots and the intent of the current utterance."""
@@ -149,13 +163,16 @@ class DialogState:
             if self.slots["pricerange"] is None:
                 return "4"
         if self.history_states[-1] in ("1", "2", "3.1", "3", "4"):
+            # If no restaurant in DB matches the user's preferences, go to state 6
             if self.lookup() is None:
                 return "6"
             else:
                 return "5"
         if self.history_states[-1] in ("5", "6", "7"):
+            # If the user wants to know more about the restaurant, go to state 7
             if self.history_intents[-1] == "request":
                 return "7"
+            # If the user wants to end the dialog, go to state 8
             if self.history_intents[-1] == "bye":
                 print(f"8. Goodbye and have a nice day!")
                 exit()
@@ -175,7 +192,7 @@ class DialogState:
         self.restaurants = df_output["restaurantname"].values
         return self.restaurants
 
-    def recognize_keyword(self, key, max_err) -> str:
+    def recognize_keyword(self, key: str) -> str:
         """Recognizes the keyword that is closest to the key in terms of Levenshtein distance."""
         res_word = None  # Resulting word
         res_dist = self.max_lev_distance  # Resulting distance
@@ -198,7 +215,7 @@ class DialogState:
 if __name__ == "__main__":
     dialog_state = DialogState()
     # print(dialog_state)
-    dialog_state.act("I'm looking for a brimish food")
+    dialog_state.act("I'm looking for a cheap brimish food")
     # print(dialog_state)
     dialog_state.act("I'm looking for a restaurant in the centre")
     # print(dialog_state)

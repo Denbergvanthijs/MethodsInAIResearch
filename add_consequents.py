253bc9eaa722cf5d8efae52297fea3aa0981d768
Denbergvanthijs <denbergvanthijs@gmail.com>
2022-09-27 16:27:11 +0200

    Linting and formatting

--- add_consequents.py
+++ add_consequents.py
@@ -1,44 +1,48 @@
-import csv
 import pandas as pd
-import random
-"""Add 3 columns based on handwritten rules for if the restaurant
-is romantic, touristic and child friendly."""
 
-df = pd.read_csv('restaurant_data.csv')
-t,c,r = [], [], []
 
 def touristic(price, food):
-  if price == 'cheap':
-    return True
-  elif price == 'moderate' and food == 'good':
-    return True
-  return False
+    if price == "cheap":
+        return True
+    elif price == "moderate" and food == "good":
+        return True
+    else:
+        return False
+
 
 def child_friendly(stay):
-  if stay == 'short':
-    return True
-  return False
+    if stay == "short":
+        return True
+    else:
+        return False
+
 
 def romantic(stay, food, price, crowdedness):
-  if stay == 'long' and crowdedness == 'not busy':
-    return True
-  elif stay == 'long' and crowdedness == 'not busy':
-    return True
-  return False
+    if stay == "long" and crowdedness == "not busy":
+        return True
+    elif stay == "long" and crowdedness == "not busy":
+        return True
+    else:
+        return False
+
 
 def add_rules(dataframe):
-  for index, row in dataframe.iterrows():
-    t.append(touristic(row['pricerange'],row['foodquality']))
-    c.append(child_friendly(row['lengthofstay']))
-    r.append(romantic(row['lengthofstay'],row['foodquality'],row['pricerange'],row['crowdedness']))
-  dataframe =dataframe.assign(touristic=t,child_friendly=c,romantic=r)
-  return(dataframe)
+    """Add 3 columns based on handwritten rules for if the restaurant
+    is romantic, touristic and child friendly."""
+    t, c, r = [], [], []
+    for _, row in dataframe.iterrows():
+        t.append(touristic(row["pricerange"], row["foodquality"]))
+        c.append(child_friendly(row["lengthofstay"]))
+        r.append(romantic(row["lengthofstay"], row["foodquality"], row["pricerange"], row["crowdedness"]))
+    dataframe = dataframe.assign(touristic=t, child_friendly=c, romantic=r)
+    return (dataframe)
+
 
 if __name__ == "__main__":
     # Add consequents and add to restaurant_data csv file
     df = pd.read_csv("./data/restaurant_data.csv")
     df_add = add_rules(df)
-    df_add.to_csv("./data/restaurantdata.csv")
+    df_add.to_csv("./data/restaurant_data_consequents.csv")
 
-# Check if there are romantic restaurants
-# df_add.loc[(df_add['romantic'] == True)]
+    # Check if there are romantic restaurants
+    print(df_add.loc[(df_add["romantic"] == True)])

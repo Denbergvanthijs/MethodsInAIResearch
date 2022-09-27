49beaa877b7530211f43029cd5b91a481785f140
Denbergvanthijs <denbergvanthijs@gmail.com>
2022-09-27 13:42:36 +0200

    Linting and formatting

--- add_columns.py
+++ add_columns.py
@@ -1,22 +1,27 @@
-import csv
-import pandas as pd
 import random
-"""Add columns to the dataframe based on the consequents.
-In our case, this is food quliaty, crowdedness and lengthofstay.
-The variables are added randomly."""
 
-df = pd.read_csv('restaurant_info.csv')
-food_quality, crowded, lengthstay = [], [], []
-food, crowd, length = ['good', 'acceptable'],['busy', 'not busy'],['long', 'short']
+import pandas as pd
 
-def add_random_columns(dataframe):
-  for i in range(len(dataframe)):
-    food_quality.append(random.choice(food))
-    crowded.append(random.choice(crowd))
-    lengthstay.append(random.choice(length))
-  dataframe =dataframe.assign(foodquality=food_quality,crowdedness=crowded,lengthofstay=lengthstay)
-  return(dataframe)
 
-# Add columns and add to empty csv file
-df_plus = add_random_columns(df)
-df_plus.to_csv("/restaurant_data.csv")
+def add_random_columns(dataframe: pd.DataFrame) -> pd.DataFrame:
+    """Add columns to the dataframe based on the consequents.
+    In our case, this is food quliaty, crowdedness and lengthofstay.
+    The variables are added randomly."""
+    food_quality, crowded, lengthstay = [], [], []
+    food, crowd, length = ["good", "acceptable"], ["busy", "not busy"], ["long", "short"]
+
+    for _ in range(len(dataframe)):
+        food_quality.append(random.choice(food))
+        crowded.append(random.choice(crowd))
+        lengthstay.append(random.choice(length))
+
+    dataframe = dataframe.assign(foodquality=food_quality, crowdedness=crowded, lengthofstay=lengthstay)
+
+    return dataframe
+
+
+if __name__ == "__main__":
+    # Add columns and add to empty csv file
+    df = pd.read_csv("./data/restaurant_info.csv")
+    df_plus = add_random_columns(df)
+    df_plus.to_csv("./restaurant_data.csv")
